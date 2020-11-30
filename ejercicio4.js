6b8f39aec9418302dcb4dcf8c859340e7206aba5
94manuel <manu_fer_santo@live.com.mx>
2020-11-29 23:20:57 -0500

    Reto 1 SENA

--- ejercicio4.js
+++ ejercicio4.js
@@ -9,6 +9,6 @@ function Calcularejercicio4(){
   (numeroaleatorio == 1 || numeroaleatorio == 3 || numeroaleatorio == 5 || numeroaleatorio == 7 || numeroaleatorio == 8 || numeroaleatorio == 10 || numeroaleatorio == 12 ) ? document.getElementById('ejercicio4resultado').innerHTML = "El mes "+numeroaleatorio+" tiene 31 dias." : '';
 
   (numeroaleatorio == 4 || numeroaleatorio == 6 || numeroaleatorio == 9 || numeroaleatorio == 11) ? document.getElementById('ejercicio4resultado').innerHTML = "El mes "+numeroaleatorio+" tiene 30 dias." : '';
-  (numeroaleatorio == 29) ? document.getElementById('ejercicio4resultado').innerHTML = "El mes "+numeroaleatorio+" tiene 29 dias." :'';
+  (numeroaleatorio == 2) ? document.getElementById('ejercicio4resultado').innerHTML = "El mes "+numeroaleatorio+" tiene 29 dias." :'';
 
 }
